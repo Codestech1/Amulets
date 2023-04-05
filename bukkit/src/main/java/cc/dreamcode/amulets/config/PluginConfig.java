f814a8455f19ed1487503fa3e04960785302bfb5
super <superjojominecraft@gmail.com>
2023-04-06 01:32:39 +0200

    Rename to Amulets and Prepare to work on project

---
+++ bukkit/src/main/java/cc/dreamcode/amulets/config/PluginConfig.java
@@ -0,0 +1,17 @@
+package cc.dreamcode.amulets.config;
+
+import cc.dreamcode.platform.bukkit.component.configuration.Configuration;
+import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.annotation.Header;
+import eu.okaeri.configs.annotation.NameModifier;
+import eu.okaeri.configs.annotation.NameStrategy;
+import eu.okaeri.configs.annotation.Names;
+
+@Configuration(
+        child = "config.yml"
+)
+@Header("## Dream-Amulets (Main-Config) ##")
+@Names(strategy = NameStrategy.HYPHEN_CASE, modifier = NameModifier.TO_LOWER_CASE)
+public class PluginConfig extends OkaeriConfig {
+
+}
